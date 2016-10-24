042a83baff734cf3a6847b60647dbdfa8fc3d3b8
BANSHEE\r_car <r.carvalhocc@gmail.com>
2016-10-24 22:03:24 +0100

    cenas2

--- calculator/IService1.cs
+++ calculator/IService1.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace calculator
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    //cenas
+    //cenas2
     [ServiceContract]
     public interface IService1
     {
